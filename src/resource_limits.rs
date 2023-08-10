0e93b82002130be285cd86d3901f72386d5f39ed
koenichiwa <47349524+koenichiwa@users.noreply.github.com>
2023-08-10 17:41:37 +0200

    Document resource_limits

--- src/resource_limits.rs
+++ src/resource_limits.rs
@@ -1,8 +1,9 @@
-// See the HardResourceLimits / SoftResourceLimits section of https://www.manpagez.com/man/5/launchd.plist/
-//
-
 use serde::{Deserialize, Serialize};
 
+/// Configuration options for resource limits.
+/// See the HardResourceLimits / SoftResourceLimits section of https://www.manpagez.com/man/5/launchd.plist/
+/// 
+/// This struct holds various resource limits that can be configured for a service.
 #[derive(Serialize, Deserialize, Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 #[serde(rename_all = "PascalCase")]
 pub struct ResourceLimits {
@@ -19,50 +20,60 @@ pub struct ResourceLimits {
 }
 
 impl ResourceLimits {
+    /// Creates a new `ResourceLimits` instance with default values.
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Sets the resource limit for core dump file size.
     pub fn with_core(mut self, value: u64) -> Self {
         self.core = Some(value);
         self
     }
 
+    /// Sets the resource limit for CPU usage time.
     pub fn with_cpu(mut self, value: u64) -> Self {
         self.cpu = Some(value);
         self
     }
 
+    /// Sets the resource limit for data segment size.
     pub fn with_data(mut self, value: u64) -> Self {
         self.data = Some(value);
         self
     }
 
+    /// Sets the resource limit for maximum file size.
     pub fn with_file_size(mut self, value: u64) -> Self {
         self.file_size = Some(value);
         self
     }
 
+    /// Sets the resource limit for locked memory size.
     pub fn with_memory_lock(mut self, value: u64) -> Self {
         self.memory_lock = Some(value);
         self
     }
 
+    /// Sets the resource limit for maximum number of open files.
     pub fn with_number_of_files(mut self, value: u64) -> Self {
         self.number_of_files = Some(value);
         self
     }
 
+    /// Sets the resource limit for maximum number of processes.
     pub fn with_number_of_processes(mut self, value: u64) -> Self {
         self.number_of_processes = Some(value);
         self
     }
 
+    /// Sets the resource limit for resident set size.
     pub fn with_resident_set_size(mut self, value: u64) -> Self {
         self.resident_set_size = Some(value);
         self
     }
 
+    /// Sets the resource limit for stack size.
     pub fn with_stack(mut self, value: u64) -> Self {
         self.stack = Some(value);
         self
